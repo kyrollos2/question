19c78f0fcfc443b1a6f210e9d6f712897adf016c
kyrollos2 <125082123+kyrollos2@users.noreply.github.com>
2024-02-22 07:05:33 -0500

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const yesBtn = document.querySelector(".yes-btn");
 const noBtn = document.querySelector(".no-btn");
 
 yesBtn.addEventListener("click", () => {
-  question.innerHTML = "Yay, see you  the Later!";
+  question.innerHTML = "Yay, see you later <3!";
   gif.src =
     "https://media.giphy.com/media/UMon0fuimoAN9ueUNP/giphy.gif";
 });
